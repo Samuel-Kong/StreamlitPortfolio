1d4a568268d2bae992ffbf54e09de34465398c8d
Samuel <konglexinsamuel@gmail.com>
2025-03-08 11:41:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,10 @@ st.title("Welcome to My Portfolio!")
 if st.session_state.page == "Home":
     st.header("About Me")
     st.write("""
-    Hello! I'm Kong Le Xin Samuel, a passionate developer with expertise in Python, cybersecurity, data manipulation, and machine learning. I do photography, read manga, watch anime and play games like Minecraft and CoDM in my free time.
+    Hello! I'm Kong Le Xin Samuel, a passionate developer with expertise in Python, cybersecurity, data manipulation, and machine learning. I take photographs, read manga, watch anime, and play games like Minecraft and CoDM in my free time.
+    \n
+    Connect with me on Discord! Username: entity8o8
+    Send me an email at konglexinsamuel@gmail.com
     """)
 
     st.image("images/306_SamuelKong.png", caption="That's me!", width=200)
